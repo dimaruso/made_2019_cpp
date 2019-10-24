f0e242c5d6056077c492ca6398d260e525db9ae3
dimaruso <54507670+dimaruso@users.noreply.github.com>
2019-10-24 23:34:48 +0300

    add new tests

--- 02/test.cpp
+++ 02/test.cpp
@@ -4,33 +4,42 @@
 int main() {
     LinearAllocator q(100);
 
-    char *a1 = q.alloc(10);
-    assert(a1 != nullptr);
+	char *a1 = q.alloc(10);
+	assert(a1 != nullptr);
 
-    char *a2 = q.alloc(20);
-    assert(a2 != nullptr);
-    assert(a2 - a1 == 20);
+	char *a2 = q.alloc(20);
+	assert(a2 != nullptr);
+	assert(a2 - a1 == 20);
 
-    a2[0] = 'c';
-    assert(a2 [0] == 'c');
+	a2[0] = 'c';
+	assert(a2[0] == 'c');
 
-    q.reset();
-    assert(a2[0] != 'c');
+	q.reset();
 
-    char *a3 = q.alloc(10);
-    assert(a3 != nullptr);
+	char *a3 = q.alloc(10);
+	assert(a3 != nullptr);
 
-    char *a4 = q.alloc(20);
-    assert(a4 != nullptr);
-    assert(a4 - a3 == 20);
+	char *a4 = q.alloc(20);
+	assert(a4 != nullptr);
+	assert(a4 - a3 == 20);
 
-    a3[0] = 'c';
-    assert(a3 [0] == 'c');
+	a3[0] = 'c';
+	assert(a3[0] == 'c');
 
-    q.reset();
-    assert(a3[0] != 'c');
 
-    std::cout << "Success!" << std::endl;
-    return 0;
+	char *a5 = q.alloc(70);
+	assert(a5 != nullptr);
+
+	q.reset();
+
+	char *a6 = q.alloc(1000000);
+	assert(a6 == nullptr);
+
+	LinearAllocator q2(1000000000000000000);
+	char* a7 = q2.alloc(1);
+	assert(a7 == nullptr);
+
+	std::cout << "Success!" << std::endl;
+	return 0;
 
 }
