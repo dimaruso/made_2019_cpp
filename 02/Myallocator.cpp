dfd12f98001a8d79c03a6891cf34efbf37b63933
dimaruso <54507670+dimaruso@users.noreply.github.com>
2019-10-24 23:33:10 +0300

    Fixed limit of memory

--- 02/Myallocator.cpp
+++ 02/Myallocator.cpp
@@ -3,60 +3,43 @@
 class LinearAllocator
 {
 public:
-	//LinearAllocator();
+
 	LinearAllocator(size_t maxSize);
-	//~LinearAllocator();
 	char* alloc(size_t size);
 	void reset();
-
-	//void prin();
 
 private:
 	char* myBuffer;
 	size_t totalSize;
 	size_t currentSize;
 };
-/*
-void LinearAllocator::prin() {
 
-	for (int i = 0; i < totalSize; i++)
-		std::cout << myBuffer[i] << " ";
-	std::cout << std::endl;
-};
-
-LinearAllocator::LinearAllocator() :
-	myBuffer(new char[8]),
-	totalSize(8),
-	currentSize(0)
-{	
-}*/
-LinearAllocator::LinearAllocator(size_t maxSize):
-	myBuffer(new char[maxSize]),
+LinearAllocator::LinearAllocator(size_t maxSize) :
+	myBuffer((char *)malloc(maxSize)),
 	totalSize(maxSize),
 	currentSize(0)
 {
 }
-/*
-LinearAllocator::~LinearAllocator()
-{
-	delete[] myBuffer;
-	//free(myBuffer);
-}
-*/
+
 char* LinearAllocator::alloc(size_t size)
 {
-	if (size)
+	if (myBuffer != nullptr)
 	{
-		if (currentSize+size <= totalSize)
+		if (size)
 		{
-			currentSize += size;
-			return myBuffer+currentSize;
+			if (currentSize + size <= totalSize)
+			{
+				currentSize += size;
+				return myBuffer + currentSize;
+			}
+			else
+				return nullptr;
 		}
 		else
-			return nullptr; 
-	} 
-	else
-		return myBuffer;
+			return myBuffer;
+	}
+	else 
+		return nullptr;
 }
 
 void LinearAllocator::reset()
