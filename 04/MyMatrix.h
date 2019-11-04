5c17f2eed340a3a91a9a3f8688f0a82e924345cd
dimaruso <54507670+dimaruso@users.noreply.github.com>
2019-11-04 19:01:16 +0300

    Create proxy class Row, use vector

--- 04/MyMatrix.h
+++ 04/MyMatrix.h
@@ -1,15 +1,17 @@
 #pragma once
+#include <vector>
+
 class MyMatrix
 {
 public:
 	MyMatrix(size_t _rows, size_t _cols);
 	~MyMatrix();
-
+	class Row;
 	const size_t getRows() const;
 	const size_t getColumns() const;
 
-	const int* operator[](size_t i) const;
-	int* operator[](size_t i);
+	const Row& operator[](size_t i) const;
+	Row& operator[](size_t i);
 
 	bool operator==(const MyMatrix& other) const;
 	bool operator!=(const MyMatrix& other) const;
@@ -17,10 +19,38 @@ public:
 	MyMatrix& operator=(const MyMatrix& m);
 
 	MyMatrix operator*(const int k) const;
-	MyMatrix operator*=(const int k) const;
+	MyMatrix operator*=(const int k);
 
 private:
 	size_t rows;
 	size_t cols;
-	int* data;
+	std::vector<Row> Data;
+};
+
+class MyMatrix::Row {
+
+public:
+	Row(size_t _cols)
+	{
+		data.resize(_cols);
+		for (size_t i = 0; i < _cols; ++i)
+			data[i] = 0;
+	}
+	~Row()
+	{
+	}
+	const int& operator[](const int i) const
+	{
+		if (i < data.size()) return data[i];
+		else
+			throw std::out_of_range("wrong column index");
+	}
+	int& operator[](const int i) 
+	{
+		if (i < data.size()) return data[i];
+		else
+			throw std::out_of_range("wrong column index");
+	}
+private:
+	std::vector<int> data;
 };
