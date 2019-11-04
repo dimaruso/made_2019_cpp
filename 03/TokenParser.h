ff5284350a8565cf20d032b104f12f58d8f034e9
dimaruso <54507670+dimaruso@users.noreply.github.com>
2019-11-04 19:26:17 +0300

    Remove using

--- 03/TokenParser.h
+++ 03/TokenParser.h
@@ -1,6 +1,4 @@
-using callback = bool(*) (int i);
-using callbackntoken = int(*) (const int &token);
-using callbackstoken = std::string(*) (const std::string &token);
-
 void find_token(const std::string &pr, const int len,
-	callback start, callback end, callbackntoken Ntoken, callbackstoken Stoken);
+	void (*start)(int i), void(*end)(int i),
+	void (*Ntoken)(const int &token),
+	void(*Stoken)(const std::string &token));
